7dbd4fc835e775e1da70e075e2ec5284ef29c972
David Han <david0han@berkeley.edu>
2021-08-02 21:48:38 -0700

    WOOOO WEBSOCKTHS

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'fittingroom',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+ASGI_APPLICATION = 'finalproject.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
