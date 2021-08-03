a5d56daa0e1ad7afc33e465a3eb360eec4885a6d
Savanna <vuksavanna@berkeley.edu>
2021-08-02 20:32:53 -0700

    html templates

--- finalproject/fittingroom/urls.py
+++ finalproject/fittingroom/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('main', views.main, name='main'),
+    path('resources', views.resources, name='resources'),
+    path('room', views.room, name='room'),
+    path('select-custom', views.select_custom, name='select_custom'),
+    path('select-pre', views.select_pre, name='select_pre'),
 ]
--- finalproject/fittingroom/views.py
+++ finalproject/fittingroom/views.py
@@ -6,3 +6,16 @@ def index(request):
 
 def main(request):
     return render(request, 'fittingroom/main.html')
+
+def resources(request):
+    return render(request, 'fittingroom/resources.html')
+
+def room(request):
+    return render(request, 'fittingroom/room.html')
+
+def select_custom(request):
+    return render(request, 'fittingroom/select-custom.html')
+
+def select_pre(request):
+    return render(request, 'fittingroom/select-pre.html')
+
