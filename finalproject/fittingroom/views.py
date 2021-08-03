b51c3bba2914d2f64be363ce1ca256ac7bdf2846
Kathy Hoang <khoang21@berkeley.edu>
2021-08-02 18:56:26 -0700

    added main and basic styling

--- finalproject/fittingroom/views.py
+++ finalproject/fittingroom/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'fittingroom/index.html')
+
+def main(request):
+    return render(request, 'fittingroom/main.html')
